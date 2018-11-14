41ced66deabb2c76c1bd45a628f850ab86bb0820
Glen Hunter <ghunter@sonatype.com>
2018-11-14 13:48:12 -0500

    Revert "Changed default font size"
    This reverts commit fbdddca7e5f34cd07c451974aa7d95e926440553.

--- config.js
+++ config.js
@@ -41,9 +41,7 @@ export default angular.module('config', [iqFormLayoutModule.name, iqModalModule.
     })
     
     .constant('userGuideConfig', {
-      'How to add a page': 'user-guide/how-to-add-a-page.html',
       'BEM': 'user-guide/bem.html',
-      'NX Namespace': 'user-guide/nx-namespace.html',
       'Code Styles': 'user-guide/code-style.html'
     })
 
